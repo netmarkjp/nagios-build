5927e77b0ae76856af899701c8e581d1b22562ed
Toshiaki Baba <toshiaki@netmark.jp>
2016-12-01 22:25:16 +0900

    bump up to 4.2.3

--- docker_centos7/run.sh
+++ docker_centos7/run.sh
@@ -2,15 +2,13 @@
 
 set -e
 
-NAGIOS_VERSION=${1:-4.1.0}
+NAGIOS_VERSION=${1:-4.2.3}
 
 echo NAGIOS_VERSION: ${NAGIOS_VERSION:?}
 
 cd /tmp
-curl -kLO https://github.com/NagiosEnterprises/nagioscore/archive/nagios-${NAGIOS_VERSION}.tar.gz
-tar zxf nagios-${NAGIOS_VERSION}.tar.gz
-mv nagioscore-nagios-${NAGIOS_VERSION} nagios-${NAGIOS_VERSION}
+curl -kL https://github.com/NagiosEnterprises/nagioscore/archive/release-${NAGIOS_VERSION}.tar.gz | tar zxf -
+mv nagioscore-release-${NAGIOS_VERSION} nagios-${NAGIOS_VERSION}
 tar zcfp /root/rpmbuild/SOURCES/nagios-${NAGIOS_VERSION}.tar.gz nagios-${NAGIOS_VERSION}
 rpmbuild -bb /tmp/nagios-${NAGIOS_VERSION}/nagios.spec
 cp -a /root/rpmbuild/RPMS/* /dest/.
-
